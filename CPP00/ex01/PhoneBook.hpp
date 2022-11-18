62a5a01b20d7ca11b0d75effb87417aa47fcdd89
loundor <loundor@gmail.com>
2022-11-18 18:56:22 +0100

    OK

--- CPP00/ex01/PhoneBook.hpp
+++ CPP00/ex01/PhoneBook.hpp
@@ -1,7 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stissera <stissera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 14:46:30 by stissera          #+#    #+#             */
+/*   Updated: 2022/11/18 14:46:55 by stissera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
+
 #include <iostream>
 #include <iomanip>
+
 #ifndef CONTACT_HPP
 	#include "Contact.hpp"
 #endif
@@ -19,7 +33,7 @@ class PhoneBook{
 		int			total;
 
 	private:
-		Contact _rep[8];
+		Contact 	_rep[8];
 };
 
 #endif
